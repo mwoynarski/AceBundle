d7409b21d00d9d5758566762f26dbbf6f510ab77
Nathanael d. Noblet <nathanael@noblet.ca>
2016-09-12 13:39:52 -0600

    simplify / fix the tests

--- Tests/Form/AutocompleterTypeTest.php
+++ Tests/Form/AutocompleterTypeTest.php
@@ -2,15 +2,12 @@
 
 namespace NS\AceBundle\Tests\Form;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use \NS\AceBundle\Form\AutocompleterType;
 use \NS\AceBundle\Tests\BaseFormTestType;
 use \NS\AceBundle\Tests\Form\Fixtures\Entity;
-use Symfony\Component\Form\Forms;
 use Symfony\Component\Form\PreloadedExtension;
-use Symfony\Component\Form\CallbackTransformer;
-use \Doctrine\Common\Collections\Collection;
-use \Doctrine\Common\Collections\ArrayCollection;
-use \Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Description of AutocompleterTypeTest
@@ -19,13 +16,34 @@ use \Symfony\Component\Form\Exception\UnexpectedTypeException;
  */
 class AutocompleterTypeTest extends BaseFormTestType
 {
-    private $entityMgrTest;
-    public function resetFactory($entityMgr,$router = null)
+    /** @var ObjectManager */
+    private $entityMgr;
+
+    /** @var  RouterInterface */
+    private $router;
+
+    protected function setUp()
     {
-        $type = new AutocompleterType($entityMgr,$router);
-        $this->factory = Forms::createFormFactoryBuilder()
-            ->addExtensions(array(new PreloadedExtension(array($type),array())))
-            ->getFormFactory();
+        $this->router = $this->getMockBuilder('\Symfony\Component\Routing\RouterInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(array('generate', 'match', 'getRouteCollection', 'setContext', 'getContext'))
+            ->getMock();
+
+        $this->entityMgr = $this->getMockBuilder('Doctrine\Common\Persistence\ObjectManager')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getReference', 'find', 'persist', 'merge', 'clear',
+                'detach', 'refresh', 'flush', 'getRepository', 'remove', 'getClassMetadata',
+                'getMetadataFactory', 'initializeObject', 'contains'))
+            ->getMock();
+
+        parent::setUp();
+    }
+
+    protected function getExtensions()
+    {
+        $type = new AutocompleterType($this->entityMgr, $this->router);
+
+        return array(new PreloadedExtension(array($type),array()));
     }
 
     public function testFormTypeEntityToJson()
@@ -34,19 +52,15 @@ class AutocompleterTypeTest extends BaseFormTestType
         $className = get_class($entity);
         $formData  = array('auto' => '1');
         $routeName = 'routename';
-        $router    = $this->getRouter();
-        $router->expects($this->once())
+        $this->router->expects($this->once())
             ->method('generate')
             ->with($routeName)
             ->willReturn('/route/name');
 
-        $entityMgr = $this->getEntityManager();
-        $entityMgr->expects($this->once())
+        $this->entityMgr->expects($this->once())
             ->method('getReference')
             ->with($className, 1)
             ->willReturn($entity);
-
-        $this->resetFactory($entityMgr,$router);
 
         $form = $this->factory
             ->createBuilder()
@@ -76,19 +90,15 @@ class AutocompleterTypeTest extends BaseFormTestType
         $className = get_class($entities[0]);
         $formData  = array('auto' => '1,2');
         $routeName = 'routename';
-        $router    = $this->getRouter();
 
-        $entityMgr = $this->getEntityManager();
-        $entityMgr->expects($this->at(0))
+        $this->entityMgr->expects($this->at(0))
             ->method('getReference')
             ->with($className, 1)
             ->willReturn($entities[0]);
-        $entityMgr->expects($this->at(1))
+        $this->entityMgr->expects($this->at(1))
             ->method('getReference')
             ->with($className, 2)
             ->willReturn($entities[1]);
-
-        $this->resetFactory($entityMgr,$router);
 
         $form = $this->factory
             ->createBuilder()
@@ -109,42 +119,27 @@ class AutocompleterTypeTest extends BaseFormTestType
         $this->assertEquals($view['auto']->vars['attr']['data-autocompleteUrl'], $routeName);
     }
 
+    /**
+     * @group customTransformer
+     */
     public function testFormTypeCustomTransformer()
     {
-
         $entities = array(new Entity(1), new Entity(2));
 
         $className = get_class($entities[0]);
         $formData  = array('auto' => '1,2');
         $routeName = 'routename';
-        $router    = $this->getRouter();
-
-        $entityMgr = $this->getEntityManager();
-        $this->entityMgrTest = $entityMgr;
-        $entityMgr->expects($this->at(0))
-            ->method('getReference')
-            ->with($className, 1)
-            ->willReturn($entities[0]);
-        $entityMgr->expects($this->at(1))
-            ->method('getReference')
-            ->with($className, 2)
-            ->willReturn($entities[1]);
-
-        $this->resetFactory($entityMgr,$router);
-
-        //$customTransformer = $this->getCustomTransformer();
         $jsonStr     = sprintf("%d,%d", $entities[0]->getId(), $entities[1]->getId());
-        $idsArray = explode(',', $jsonStr);
-        $idsArray = array_walk($idsArray, array($this, 'walk'));
+
         $customTransformer = $this->getMockBuilder('Symfony\Component\Form\DataTransformerInterface')
-        ->disableOriginalConstructor()
-        ->getMock();
+            ->disableOriginalConstructor()
+            ->getMock();
         $customTransformer->expects($this->any())
-        ->method('transform')
-        ->willReturn($jsonStr);
+            ->method('transform')
+            ->willReturn($jsonStr);
         $customTransformer->expects($this->any())
-        ->method('reverseTransform')
-        ->willReturn($idsArray);
+            ->method('reverseTransform')
+            ->willReturn($entities);
 
         $form = $this->factory
             ->createBuilder()
@@ -159,7 +154,7 @@ class AutocompleterTypeTest extends BaseFormTestType
         $form->submit($formData);
         $data = $form['auto']->getData();
 
-        $this->assertEquals($data, $entities);
+        $this->assertEquals($entities, $data);
         $this->assertArrayHasKey('auto', $view);
         $this->assertArrayHasKey('attr', $view['auto']->vars);
         $this->assertArrayHasKey('data-autocompleteUrl', $view['auto']->vars['attr']);
@@ -175,11 +170,7 @@ class AutocompleterTypeTest extends BaseFormTestType
             'class'    => $className,
         );
 
-        $router    = $this->getRouter();
-        $entityMgr = $this->getEntityManager();
-        $this->resetFactory($entityMgr,$router);
-
-        $form      = $this->factory
+        $form = $this->factory
             ->createBuilder()
             ->add('auto', AutocompleterType::class, $properties)
             ->getForm();
@@ -195,10 +186,6 @@ class AutocompleterTypeTest extends BaseFormTestType
     {
         $entity    = new Entity(1);
         $className = get_class($entity);
-
-        $router    = $this->getRouter();
-        $entityMgr = $this->getEntityManager();
-        $this->resetFactory($entityMgr,$router);
 
         $form = $this->factory
             ->createBuilder()
@@ -216,12 +203,10 @@ class AutocompleterTypeTest extends BaseFormTestType
     {
         $entity    = new Entity(1);
         $className = get_class($entity);
-        $entityMgr = $this->getEntityManager();
 
-        $entityMgr->expects($this->never())
+        $this->entityMgr
+            ->expects($this->never())
             ->method('getReference');
-
-        $this->resetFactory($entityMgr);
 
         $form = $this->factory
             ->createBuilder()
@@ -231,58 +216,5 @@ class AutocompleterTypeTest extends BaseFormTestType
         $form->submit(array());
         $data = $form['auto']->getData();
         $this->assertNull($data);
-    }
-
-    private function getRouter()
-    {
-        return $this->getMockBuilder('\Symfony\Component\Routing\RouterInterface')
-                ->disableOriginalConstructor()
-                ->setMethods(array('generate', 'match', 'getRouteCollection', 'setContext', 'getContext'))
-                ->getMock()
-        ;
-    }
-
-    private function getEntityManager()
-    {
-        return $this->getMockBuilder('Doctrine\Common\Persistence\ObjectManager')
-                ->disableOriginalConstructor()
-                ->setMethods(array('getReference', 'find', 'persist', 'merge', 'clear',
-                    'detach', 'refresh', 'flush', 'getRepository', 'remove', 'getClassMetadata',
-                    'getMetadataFactory', 'initializeObject', 'contains'))
-                ->getMock();
-    }
-
-    private function walk(&$item, $key)
-    {
-        $entity    = new Entity(1);
-        $className = get_class($entity);
-        $entityMgr = $this->entityMgrTest ;
-        $item = $entityMgr->getReference($className, $item);
-    }
-
-    private function getCustomTransformer(){
-      return new CallbackTransformer(
-          function ($values) {
-            if (!$values) { return null; }
-            if (null === $values || empty($values)) { return ""; }
-            if (!$values instanceof Collection && !is_array($values)) {
-              throw new UnexpectedTypeException($values, 'PersistentCollection or ArrayCollection');
-            }
-            $idsArray = array();
-            foreach ($values as $entity) {
-              $idsArray[] = array('id' => $entity->getId(), 'name' => 'Does not matter', 'activefirm' => 'Does not matter');
-            }
-            if (empty($idsArray)) { return null; }
-            return json_encode($idsArray);
-          },
-          function ($ids) {
-            if ('' === $ids || null === $ids || empty($ids)) { return new ArrayCollection(); }
-            if (!is_string($ids)) { throw new UnexpectedTypeException($ids, 'string'); }
-            $idsArray = explode(',', $ids);
-            if (empty($idsArray)) { return new ArrayCollection(); }
-            array_walk($idsArray, array($this, 'walk'));
-            return $idsArray;
-          }
-        );
     }
 }
